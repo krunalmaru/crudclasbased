16f9af4bfaf7c618635df27111669dbe45e08476
krunal <krunal.maru1106@gmail.com>
2023-01-02 16:21:50 +0530

    filed id display

--- crudproject/myapp/admin.py
+++ crudproject/myapp/admin.py
@@ -4,4 +4,4 @@ from .models import User
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
-    list_display = ['name','email','password']
+    list_display = ['id','name','email','password']
