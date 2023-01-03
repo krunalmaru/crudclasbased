a30a75de4c59b725f3c57c9247a0e17b866d909f
krunal <krunal.maru1106@gmail.com>
2023-01-03 14:25:55 +0530

    templateView for render template

--- crudproject/mycontact/urls.py
+++ crudproject/mycontact/urls.py
@@ -3,5 +3,6 @@ from mycontact import views
 
 urlpatterns = [
     path('contact', views.ContactFormView.as_view(),name='contact'),
+    path('thankyu/',views.ThankyuTemplatesView.as_view(),name='thankyu'),
 
 ]
--- crudproject/mycontact/views.py
+++ crudproject/mycontact/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from .forms import ContactForm
+from django.views.generic.base import TemplateView,View,RedirectView
 from django.views.generic.edit import FormView 
 # Create your views here.
 
 class ContactFormView(FormView):
     template_name = 'mycontact/contact.html'
     form_class = ContactForm
+    success_url = '/thankyu/'
+
+class ThankyuTemplatesView(TemplateView):
+    template_name = 'mycontact/thankyu.html'
+     
