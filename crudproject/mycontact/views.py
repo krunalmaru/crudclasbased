9833a16d62c8d878e5f38df0388e2ba7f39ed020
krunal <krunal.maru1106@gmail.com>
2023-01-03 15:22:27 +0530

    initial data and form data

--- crudproject/mycontact/views.py
+++ crudproject/mycontact/views.py
@@ -1,13 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render,HttpResponseRedirect,HttpResponse
 from .forms import ContactForm
 from django.views.generic.base import TemplateView,View,RedirectView
 from django.views.generic.edit import FormView 
+
 # Create your views here.
 
 class ContactFormView(FormView):
     template_name = 'mycontact/contact.html'
     form_class = ContactForm
     success_url = '/thankyu/'
+    initial = {'name':'your Name','email':'rahul@gmail.com','message':'your Message'}
+    def form_valid(self, form):
+        print(form)
+        print(form.cleaned_data['name'])
+        print(form.cleaned_data['email'])
+        print(form.cleaned_data['message'])
+        # return super().form_valid(form)
+        return HttpResponse('msg sent')
 
 class ThankyuTemplatesView(TemplateView):
     template_name = 'mycontact/thankyu.html'
