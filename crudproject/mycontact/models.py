0b61a51b00d958178cf8ec2765ec4719ba3d3cdf
krunal <krunal.maru1106@gmail.com>
2023-01-03 16:42:21 +0530

    createview

--- crudproject/mycontact/models.py
+++ crudproject/mycontact/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.urls import reverse
 # Create your models here.
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    password = models.CharField(max_length=20)
+
+    # def get_absolute_url(self):
+    #     return reverse("thankyu")
+    def get_absolute_url(self):
+        return reverse("studentdetails", kwargs={"pk": self.pk})
+    
